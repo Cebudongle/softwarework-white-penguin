1b567b798f08197bd2f11703d1172843a6a34e3c
E0ieo <1052221246@qq.com>
2021-11-16 20:39:00 +0800

    third

--- Wechat/pages/demo2/demo2.js
+++ Wechat/pages/demo2/demo2.js
@@ -56,6 +56,7 @@ Page({
                 pointNum:'1000',
                 like: true,
                 islike: false,
+                match:false,
             },
         ],
         // 关键词展示
@@ -79,9 +80,69 @@ Page({
             value: ''
         })
     },
+    // onReachBottom: function () {
+    //     console.log('加载更多')
+    //     setTimeout(() => {
+    //       this.setData({
+    //         isHideLoadMore: true,
+    //         recommends: [
+    //           {
+    //             goodId: 7,
+    //             name: 'OLAY玉兰油精油沐浴露玫瑰滋养二合一450ml',
+    //             url: 'bill',
+    //             imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161213/148162245074.jpg',
+    //             newprice: "￥36.60",
+    //             oldprice: "￥40.00",
+    //           },
+    //           {
+    //             goodId: 10,
+    //             name: '兰蔻玫瑰清滢保湿柔肤水嫩肤水化妆水400ml补水保湿温和不刺激',
+    //             url: 'bill',
+    //             imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057937593.jpg',
+    //             newprice: "￥30.00",
+    //             oldprice: "￥80.00",
+    //           }, {
+    //             goodId: 11,
+    //             name: 'Lancome/兰蔻清莹柔肤爽肤水/粉水400ml补水保湿玫瑰水化妆水',
+    //             url: 'bill',
+    //             imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057953326.jpg',
+    //             newprice: "￥30.00",
+    //             oldprice: "￥80.00",
+    //           },
+    //           {
+    //             goodId: 12,
+    //             name: '美国CLINIQUE倩碧黄油无油/特效润肤露125ml',
+    //             url: 'bill',
+    //             imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/14805828016.jpg',
+    //             newprice: "￥239.00",
+    //             oldprice: "￥320.00",
+    //           },
+    //           {
+    //             goodId: 13,
+    //             name: '法国LANCOME兰蔻柔皙轻透隔离防晒乳霜50ML2017年3月到期',
+    //             url: 'bill',
+    //             imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058014894.jpg',
+    //             newprice: "￥130.00",
+    //             oldprice: "￥180.00",
+    //           },
+    //         ],
+    //       })
+    //     }, 1000)
+    //   },
+    
+    //     onPullDownRefresh:function()
+    //     {
+    //       wx.showNavigationBarLoading() //在标题栏中显示加载
+    //       //模拟加载
+    //       setTimeout(function()
+    //       {
+    //         // complete
+    //         wx.hideNavigationBarLoading() //完成停止加载
+    //         wx.stopPullDownRefresh() //停止下拉刷新
+    //       },1500);
+    //     },
    // 输入操作
     searchInput(e) {
-        console.log(e);
         // 判断是否为空，为空showKeywords:false
         if(!e.detail.value){
             this.setData({
@@ -105,11 +166,57 @@ Page({
             }
         }
     },
+    searchconfirm(e){
+        const text = e.detail.value;
+        let keywords = this.data.keywords;
+        // 判断是否为空，为空showKeywords:false
+        if(!e.detail.value){
+            this.setData({
+                showKeywords: false,
+                isSearch:true,
+                isNULL:true,
+            })
+        }
+       // 不为空时 showKeywords: true
+        else{
+            this.setData({
+                value:text,
+                isSearch:false,
+                showKeywords:false
+            })
+            console.log(this.data.value);
+            this.historyHandle(text);
+            var i;
+            for(i=0;i<keywords.length;i++)
+            {
+                if(keywords[i]==text)
+                {
+                    this.setData({
+                        match:true,
+                        showResult:true,
+                        showKeywords:false
+                    })
+                    console.log(i);
+                    break;
+                }
+            }
+            console.log(i);
+            if(i==keywords.length)
+            {
+                this.setData({
+                    showResult: true,
+                    match:false,
+                    showKeywords:false
+                })
+            }
+        }
+    },
 // 获取输入信息然后设置showKeywords: false, showResult: true
     keywordHandle(e) {
         const text = e.target.dataset.text;
         this.setData({
             value: text,
+            match:true,
             showKeywords: false,
             showResult: true
         })
@@ -185,3 +292,5 @@ Page({
         }
     }
 })
+
+
